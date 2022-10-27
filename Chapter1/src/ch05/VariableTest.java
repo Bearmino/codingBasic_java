411de657773feedc929a45f3e6fd9c2e1d73d215
Bearmino <minho51y@gmail.com>
2022-10-27 13:13:47 +0900

    StartJava

---
+++ Chapter1/src/ch05/VariableTest.java
@@ -0,0 +1,16 @@
+package ch05;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age, count;
+		age=10;
+		count=20;
+		
+		int level = 10000;
+		System.out.println(age);
+		System.out.println(count);
+		System.out.println(level);
+	}
+
+}
