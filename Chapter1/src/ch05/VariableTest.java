9fce747f5f3bffc095c37d7e013e369d16be25a6
Bearmino <minho51y@gmail.com>
2022-10-27 13:58:42 +0900

    VariableTest

--- Chapter1/src/ch05/VariableTest.java
+++ Chapter1/src/ch05/VariableTest.java
@@ -7,7 +7,7 @@ public class VariableTest {
 		age=10;
 		count=20;
 		
-		int level = 10000;
+		int level = 1000;
 		System.out.println(age);
 		System.out.println(count);
 		System.out.println(level);
