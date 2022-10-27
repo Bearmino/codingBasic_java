2f7da3c264c00fc056c9b11b71e48ffd23d56d66
Bearmino <minho51y@gmail.com>
2022-10-27 15:53:28 +0900

    Constant

--- Chapter1/src/ch10/Constant.java
+++ Chapter1/src/ch10/Constant.java
@@ -14,5 +14,12 @@ public class Constant {
 		System.out.println(MAX_NUM);
 		System.out.println(MIN_NUM);
 		
+		/*리터널(literal)
+		 * 프로그램에서 사용하는 숫자,문자,논리값을 말함
+		 * 리터럴은 상수 풀(constant Pool)에 있음
+		 * 정수 리터럴은 int로, 실수 리터럴은 double로 저장됨
+		 * 정수의 범위가 넘어가는 경우 L(long)으로, float을 사용하는 경우는 F,f 식별자를 써줘야 한다.
+		 */
+		
 	}
 }
