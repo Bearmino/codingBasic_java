669a89e415b509e44a5793db87d8c9d9cfa9a592
Bearmino <minho51y@gmail.com>
2022-10-27 14:54:10 +0900

    Constant

---
+++ Chapter1/src/ch10/Constant.java
@@ -0,0 +1,18 @@
+package ch10;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		
+		final int MAX_NUM=100;
+		final int MIN_NUM=10;
+		
+		//MIN_NUM=0;
+		//MAX_NUM=1000;
+		//상수는 변하지 않는 값으로 final 예약어 사용
+		
+		System.out.println(MAX_NUM);
+		System.out.println(MIN_NUM);
+		
+	}
+}
