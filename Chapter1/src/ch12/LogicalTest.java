c92e43f8b7fc318cb5fddb4563a5e85157c5fb58
Bearmino <minho51y@gmail.com>
2022-10-31 11:05:07 +0900

    Logical

--- Chapter1/src/ch12/LogicalTest.java
+++ Chapter1/src/ch12/LogicalTest.java
@@ -12,18 +12,18 @@ public class LogicalTest {
 		
 		flag = (num1<0) && (num2<0);
 		System.out.println(flag);
-		//&&(논리곱)은 두 항의 결과가 모두 true일때만 결과가 true
+		//두 항이 모두 참인 경우에만 결과값이 참
 		
 		flag = (num1>0) || (num2>0);
 		System.out.println(flag);
-		//||(논리합)은 두 항의 결과가 모두 false일때만 결과가 false
+		//두 항 중 하나의 항만 참이면, 결과값은 참
 		
 		flag = (num1<0) || (num2<0);
 		System.out.println(flag);
 		
 		flag = !(num1>0);
 		System.out.println(flag);
-		
+		//부정 단항 연산자. 참인 경우는 거짓으로 거짓은 참으로 변환
 		
 	}
 
