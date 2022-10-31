65ccee46310710a45f5b0de7bccfdf4b4c991574
Bearmino <minho51y@gmail.com>
2022-10-31 13:01:32 +0900

    IfElse

---
+++ Chapter1/src/ch14/IfElseTest.java
@@ -0,0 +1,32 @@
+package ch14;
+
+import java.util.Scanner;
+
+public class IfElseTest {
+
+	public static void main(String[] args) {
+		
+		int age=7;
+		
+		//Scanner scanner = new Scanner(System.in);
+		//int age = scanner.nextInt();
+		//System.out.println("당신은 몇살 입니까? ");
+		
+		if(age >= 8) {
+			
+			System.out.println("학교에 다닙니다.");
+		}else {
+			System.out.println("학교에 다니지 않습니다.");
+		}
+		
+	/* if 조건식
+	 * if(조건식){
+	 * 	조건식이 '참'인 경우 수행
+	 * }
+	 * else {
+	 *  조건식이 '거짓'인 경우 수행
+	 * }
+	 */
+	}
+
+}
