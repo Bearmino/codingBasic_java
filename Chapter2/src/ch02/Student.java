73a7c90112cd532845da03d3052aad3e5a2bcf04
Bearmino <minho51y@gmail.com>
2022-11-09 13:13:34 +0900

    Object

--- Chapter2/src/ch02/Student.java
+++ Chapter2/src/ch02/Student.java
@@ -1,10 +1,25 @@
 package ch02;
-
-public class order {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+/*
+ * 객체(Object)
+ * 의사나 행위가 미치는 대상
+ * 구체적,추상적 데 이터의 단위(학생,학위,생산 등...)
+ * 
+ * 절차 지향 프로그래밍
+ * 시간이나 사건의 흐름에 따른 프로그래밍
+ * 
+ * 객체 지향 프로그램의 구성방법
+ * 1.객체를 정의
+ * 2.각 객체의 제공 기능 구현
+ * 3.각 객체가 제공하는 기능들 간의 소통을 통한 객체간의 협력을 구현
+ * 
+ * */
+// 학생클래스
+public class Student {
+	
+	int studentNumber;
+	String studentName;
+	int majorCode;
+	String majorName;
+	int greade;
 
 }
---
+++ Chapter2/src/ch02/UserInfo.java
@@ -0,0 +1,12 @@
+package ch02;
+
+//UserInfo
+public class UserInfo {
+	
+	String userId;
+	String userPassword;
+	String userName;
+	String userAddress;
+	int phoneNumber;
+	
+}
