641df24654158a83e8632c84d0905406690b9335
Bearmino <minho51y@gmail.com>
2022-11-09 13:50:06 +0900

    userinfo

--- Chapter2/src/ch02/UserInfo.java
+++ Chapter2/src/ch02/UserInfo.java
@@ -1,6 +1,13 @@
 package ch02;
+/*
+ * 클래스 정의시
+ * 1. 클래스는 대문자로 시작하는게 좋음
+ * 2. java파일 하나에 클래스는 여러개가 있을 수 있지만, public클래스는 하나고 
+ *    public클래스와 .java파일의 이름은 동일함
+ * 3. camel notation 방식으로 명명
+ * */
 
-//유저정보 클래스 정의
+//유저정보 클래스
 public class UserInfo {
 	
 	String userId;
