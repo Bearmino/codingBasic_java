a0a76131268ae08c4dd91188387c1911e2430cca
Bearmino <minho51y@gmail.com>
2022-11-11 10:45:47 +0900

    studentTest

--- Chapter2/src/ch04/StudentTest.java
+++ Chapter2/src/ch04/StudentTest.java
@@ -8,20 +8,20 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 		
-		Student studentLee = new Student(); // studentLee라는 객체를 선언
-		studentLee.studentID = 1574875;
-		studentLee.studentName = "신동석";
-		studentLee.address = "성남시";
+		Student studentshin = new Student(); // studentLee라는 객체를 선언
+		studentshin.studentID = 1574875;
+		studentshin.studentName = "신동석";
+		studentshin.address = "성남시";
 		
-		studentLee.showStudentInfo(); //StudentLee에 대한 정보값 출력
+		studentshin.showStudentInfo(); //StudentLee에 대한 정보값 출력
 		
-		Student studentKim = new Student(); // studentKim이라는 객체를 선언
+		Student studentOh = new Student(); // studentKim이라는 객체를 선언
 		
-		studentKim.studentID = 1575178;
-		studentKim.studentName = "오민식";
-		studentKim.address = "용인시";
+		studentOh.studentID = 1575178;
+		studentOh.studentName = "오민식";
+		studentOh.address = "용인시";
 		
-		studentKim.showStudentInfo(); //StudentKim에 대한 정보값 출력
+		studentOh.showStudentInfo(); //StudentKim에 대한 정보값 출력
 			
 	}
 
