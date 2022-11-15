651ff8606e7490794fdb7d8040381eb8c9ce233f
Bearmino <minho51y@gmail.com>
2022-11-15 10:14:43 +0900

    userinforeview

--- Chapter2/src/ch08/UserinfoReview.java
+++ Chapter2/src/ch08/UserinfoReview.java
@@ -8,18 +8,17 @@ public class UserinfoReview {
 	public int userAge;
 	public String userSex;
 	
-	
 	public UserinfoReview(int userInfoLength,int userInfoWeight, String userName, int userAge, String userSex ) {
 		this.userInfoLength=userInfoLength;
 		this.userInfoWeight=userInfoWeight;
 		this.userName=userName;
 		this.userAge=userAge;
 		this.userSex=userSex;
+		
 	}
 	
 	public String UserinfoShow() {
 		return "키가" +userInfoLength+"이고"+" 몸무게가 "+userInfoWeight+"킬로인 "+userSex+"이 있습니다."+" 이름은 "+userName+"이고,나이는 "+userAge+"입니다.";
-				
 	}
-	
+
 }
