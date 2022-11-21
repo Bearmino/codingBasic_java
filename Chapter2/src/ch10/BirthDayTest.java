61528ed10087be12765219e6524df87f1730a3f2
Bearmino <minho51y@gmail.com>
2022-11-21 10:50:46 +0900

    birthdayTest

---
+++ Chapter2/src/ch10/BirthDayTest.java
@@ -0,0 +1,16 @@
+package ch10;
+
+public class BirthDayTest {
+
+	public static void main(String[] args) {
+			
+		BirthDay date = new BirthDay();
+		
+		date.setYear(2018);
+		date.setMonth(8);
+		date.setDay(10);
+		
+		date.showDate();
+	}
+
+}
