1dcdbfc4906b7ec6435516a5f15ca27500a95583
Bearmino <minho51y@gmail.com>
2022-11-21 14:26:52 +0900

    bus

---
+++ Chapter2/src/ch14/Bus.java
@@ -0,0 +1,24 @@
+package ch14;
+
+public class Bus {
+
+	int busNumber;
+	int passengerCount;
+	int money;
+	
+	//버스번호
+	public Bus(int busNumber) {
+		this.busNumber=busNumber;
+	}
+	
+	//승차
+	public void take(int money) {
+		this.money += money;
+		passengerCount++;
+	}
+	//버스정보
+	public void showBusInfo() {
+		System.out.println(busNumber+"번 버스의 승객은 "+passengerCount+"이고, 수입은 "+money+"입니다.");
+		
+	}	
+}
