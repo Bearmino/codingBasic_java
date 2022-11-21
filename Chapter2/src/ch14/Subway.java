6acfcf1a8f017622270ff437b8d72191fe590ed0
Bearmino <minho51y@gmail.com>
2022-11-21 14:27:20 +0900

    subway

---
+++ Chapter2/src/ch14/Subway.java
@@ -0,0 +1,24 @@
+package ch14;
+
+public class Subway {
+
+	String lineName;
+	int passengerCount;
+	int money;
+	
+	//버스번호
+	public Subway(String lineName) {
+		this.lineName=lineName;
+	}
+	
+	//승차
+	public void take(int money) {
+		this.money += money;
+		passengerCount++;
+	}
+	//버스정보
+	public void showSubwayInfo() {
+		System.out.println(lineName+" 지하철의 승객은 "+passengerCount+"이고, 수입은 "+money+"입니다.");
+		
+	}	
+}
