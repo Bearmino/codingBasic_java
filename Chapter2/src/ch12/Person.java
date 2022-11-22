2e8da0f6000a1f30c9b76f6aae11aad801659711
Bearmino <minho51y@gmail.com>
2022-11-22 09:20:01 +0900

    person

--- Chapter2/src/ch12/Person.java
+++ Chapter2/src/ch12/Person.java
@@ -23,12 +23,16 @@ public class Person {
 	int age;
 	
 	public Person() {
-		this("이름없음",1); //생성자를 호출
+		this("이름없음",1); //생성자에서 다른 생성자를 호출(초기화시 주로 이용)
 	}
 	
-	public Person(String name, int age) {		
+	public Person(String name, int age) {	//이름과 나이를 매개변수로 받는 생성자 생성	
 		this.name = name;
 		this.age = age;		
+	}
+	
+	public void showPerson() {
+		System.out.println(name+","+age);
 	}
 	
 	public Person getPerson() {
@@ -40,12 +44,19 @@ public class Person {
 		Person p = new Person();
 		p.name = "Minsik";
 		p.age = 30;
+		p.showPerson();
 		
 		Person p2 = new Person();
 		p2.name = "Dongsuk";
 		p2.age = 50;
+		p2.showPerson();
 		
 		System.out.println(p);
+		//ch12.Person@36baf30c 
+		Person person2 = p.getPerson();
+		System.out.println(person2);
+		
+		
 		System.out.println(p2);
 		
 	}	
