4e4cbee164537b207077259c2842e869c64bd068
Bearmino <minho51y@gmail.com>
2022-11-25 14:23:57 +0900

    collaborationReview

---
+++ Chapter2/src/ch15/TakeTransTexi.java
@@ -0,0 +1,14 @@
+package ch15;
+
+public class TakeTransTexi {
+
+	public static void main(String[] args) {
+			Student Edward = new Student("Edward", 20000);
+			Taxi taxiWel = new Taxi("잘 간다");
+			Edward.takeTaxi(taxiWel);
+			
+			Edward.showStudent();
+			taxiWel.showTaxiInfo();
+	}
+
+}
