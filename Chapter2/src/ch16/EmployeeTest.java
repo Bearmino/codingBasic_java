3896f064627cfc910cb0ff277a9d75277c8e9860
minhokim <minho51y@knou.ac.kr>
2022-11-29 05:39:45 +0900

    employeeTest

--- Chapter2/src/ch16/EmployeeTest.java
+++ Chapter2/src/ch16/EmployeeTest.java
@@ -4,17 +4,16 @@ public class EmployeeTest {
 
 	public static void main(String[] args) {
 		
+		System.out.println(Employee.serialNum); //생성자를 통한 인스턴스를 많이 사용한다.
+
 		Employee employeeShin = new Employee();
 		employeeShin.setEmployeeName("신동석");
-		System.out.println(employeeShin.serialNum);
-		
-		
+				
 		Employee employeeOh = new Employee();
 		employeeOh.setEmployeeName("오민식");
-		employeeOh.serialNum++;
 		
-		System.out.println(employeeOh.serialNum);
-		System.out.println(employeeShin.serialNum);
+		System.out.println(employeeShin.getEmployeeName()+"님의 사번은 "+employeeShin.getEmployeeId());
+		System.out.println(employeeOh.getEmployeeName()+"님의 사번은 "+employeeOh.getEmployeeId());
 		
 	}
 
