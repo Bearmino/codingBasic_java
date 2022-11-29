9b5e00f2ba839dfaf6fa9b29e79a2e4dd4af813f
Bearmino <minho51y@gmail.com>
2022-11-29 13:14:49 +0900

    takeTransTaxi

--- Chapter2/src/ch15/TakeTransTexi.java
+++ Chapter2/src/ch15/TakeTransTexi.java
@@ -4,11 +4,18 @@ public class TakeTransTexi {
 
 	public static void main(String[] args) {
 			Student Edward = new Student("Edward", 20000);
-			Taxi taxiWel = new Taxi("잘 간다");
-			Edward.takeTaxi(taxiWel);
+			Taxi taxiGood = new Taxi("잘간다 운수");
+			Edward.takeTaxi(taxiGood);
+			
+			Student Lion = new Student("Lion", 30000);
+			Lion.takeTaxi(taxiGood);
+			
 			
 			Edward.showStudent();
-			taxiWel.showTaxiInfo();
+			Lion.showStudent();
+			
+			Edward.takeOffTaxi(taxiGood);
+			taxiGood.showTaxiInfo();
 			
 	}
 
