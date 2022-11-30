aefd6add39710c2d82b562833f5c2249966e87d7
minhokim <minho51y@knou.ac.kr>
2022-11-30 22:38:55 +0900

    employeeTest2

--- Chapter2/src/ch17/EmployeeTest2.java
+++ Chapter2/src/ch17/EmployeeTest2.java
@@ -30,9 +30,9 @@ public class EmployeeTest2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println(Employee.serialNum);
+		System.out.println(Employee.getSerialNum());
 		Employee.setSerialNum(1003);
-		System.out.println(Employee.serialNum);
+		System.out.println(Employee.getSerialNum());
 		
 	}
 
