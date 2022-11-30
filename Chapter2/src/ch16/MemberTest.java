f4aa90e7d7373982519ea06897fcad214b0469a1
Bearmino <minho51y@gmail.com>
2022-11-30 15:52:19 +0900

    memberTest

--- Chapter2/src/ch16/MemberTest.java
+++ Chapter2/src/ch16/MemberTest.java
@@ -1,15 +1,15 @@
-package ch17;
+package ch16;
 
-public class EmployeeTest {
+public class MemberTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println(Employee.serialNum); //생성자를 통한 인스턴스를 많이 사용한다.
+		System.out.println(Member.serialNum); //생성자를 통한 인스턴스를 많이 사용한다.
 
-		Employee employeeShin = new Employee();
+		Member employeeShin = new Member();
 		employeeShin.setEmployeeName("신동석");
 				
-		Employee employeeOh = new Employee();
+		Member employeeOh = new Member();
 		employeeOh.setEmployeeName("오민식");
 		
 		System.out.println(employeeShin.getEmployeeName()+"님의 사번은 "+employeeShin.getEmployeeId());
